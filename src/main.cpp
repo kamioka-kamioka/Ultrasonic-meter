4d36acf814dcbb52b55343f9540a7723f3ba42ef
kamioka-kamioka <kamishu1135@gmail.com>
2025-05-24 16:16:48 +0900

    フローチャート追加

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,18 @@
 
 #define ECHO_PIN 11
 #define TRIG_PIN 12
-#define TEMP_PIN 7
+#define TEMP_PIN A3
 
 double getDistance(double);
 double microsecondsToCentimeter(long, double);
 double getTemperature();
 
 double distance = 0;
+double distanceTotal = 0;
 double temperature = 0;
 double temperatureTotal = 0;
-int count = 0;
+int tempCount = 0;
+int distCount = 0;
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
@@ -21,21 +23,20 @@ void setup()
   pinMode(ECHO_PIN, INPUT);
   pinMode(TRIG_PIN, OUTPUT);
   lcd.init();
-  lcd.backlight();
+  // lcd.backlight();
   Serial.begin(9600);
-  delay(500);
   temperature = getTemperature();
 }
 
 void loop()
 {
+  tempCount++;
   temperatureTotal += getTemperature();
-  count++;
-  if (count == 10)
+  if (tempCount == 25)
   {
-    temperature = temperatureTotal / count;
+    temperature = temperatureTotal / tempCount;
     temperatureTotal = 0;
-    count = 0;
+    tempCount = 0;
   }
   lcd.home();
   lcd.print("TEMP:         C");
@@ -43,15 +44,22 @@ void loop()
   lcd.print(temperature);
   Serial.println(temperature);
 
+  distCount++;
+  distanceTotal += getDistance(temperature);
+  if (distCount == 5)
+  {
+    distance = distanceTotal / distCount;
+    distanceTotal = 0;
+    distCount = 0;
+  }
   lcd.setCursor(0, 1);
-  distance = getDistance(temperature);
   lcd.print("DIST:         cm");
   lcd.setCursor(6, 1);
   lcd.print(distance);
   Serial.print(distance, 4);
   Serial.println("cm");
   lcd.home();
-  delay(500);
+  delay(200);
 }
 
 double getDistance(double temp)
@@ -69,18 +77,17 @@ double getDistance(double temp)
 
 double microsecondsToCentimeter(long duration, double temp)
 {
-  double du = (double)duration * 0.5;
-  double d = du * (331.5 + 0.6 * temp) * 0.0001;
+  double d = (double)duration * 0.5 * (331.5 + 0.61 * temp) * 0.0001;
   return d;
 }
 
 double getTemperature()
 {
-  int tempReading = analogRead(TEMP_PIN);
-  analogRead(2);
+  int analogVal = analogRead(TEMP_PIN);
+  double inputVolt = double(analogVal) * (5.0 / 1024.0);
 
-  double tempK = log(10000.0 * ((1024.0 / tempReading - 1)));
-  tempK = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * tempK * tempK)) * tempK);
-  double tempC = tempK - 273.15;
-  return tempC;
+  double thermistorVal = (5.0 / inputVolt - 1) * 10000;
+  double temp = 1 / (log(thermistorVal / 10000) / 3435 + (1 / (25 + 273.15))) - 273.15;
+
+  return temp;
 }
